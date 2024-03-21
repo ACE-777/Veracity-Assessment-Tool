3e8cc9e58c36c9029c57ad0a7afe2c0331b2191b
Misha Diagilev <mishadyagilev@gmail.com>
2024-03-21 22:09:28 +0300

    add some files before PR

--- internal/toloka/builder_data.go
+++ internal/toloka/builder_data.go
@@ -22,7 +22,7 @@ type Sentence struct {
 }
 
 func NewResponseData() map[ResponseData][]Sentence {
-	f, err := os.Open("internal/toloka/tasks_from_pool_06-12-2023.tsv")
+	f, err := os.Open("internal/toloka/tasks_ready_sources.tsv")
 	if err != nil {
 		log.Printf("error while opening file: %v", err)
 	}
